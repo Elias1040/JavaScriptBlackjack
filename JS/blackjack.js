88e5b4bf31e173be10e2b333c795d840e0475dd1
1micr01 <elias11a.e@gmail.com>
2022-09-24 20:47:38 +0200

    styling and animation

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -15,7 +15,7 @@ class Card {
     }
 }
 
-var deck = function() {
+var deck = function () {
     let arr = [];
     for (let suit = 0; suit < 4; suit++) {
         for (let card = 1; card < 13; card++) {
@@ -34,39 +34,59 @@ var dealerCards = [deck.pop(), deck.pop()];
 //     DrawCard()
 // }
 
+
+
 function StartGame() {
     document.getElementById("startBtn").hidden = true;
-    userCards.forEach(x => UserCardHtml(x));
-    dealerCards.forEach(x => DealerCardHtml(x));
-    CheckPoints();
+    userCards.forEach((x, y) => setTimeout(() => UserCardHtml(x), 225 * (y + 1)));
+    dealerCards.forEach((x, y) => setTimeout(() => DealerCardHtml(x), 225 * (y + 3)));
     endGame = false;
+    CheckPoints();
+    document.getElementById("UserPoints").parentElement.hidden = false
+    document.getElementById("DealerPoints").parentElement.hidden = false
     document.getElementById("Hit").hidden = false;
     document.getElementById("Stand").hidden = false;
 }
 
+function RestartGame() {
+
+}
+
 function UserCardHtml(card) {
     let htmlCard = document.createElement("img");
-    htmlCard.className = "border border-black border-2";
+    htmlCard.className = "border border-black border-2 ms-2 me-2 slide-in-right";
     htmlCard.src = card.imageSrc;
+    htmlCard.height = 225;
+    htmlCard.width = 150;
+    htmlCard.id = `${card.value}&${card.suit}`
     document.getElementById("UserSide").appendChild(htmlCard);
 }
 
 function DealerCardHtml(card) {
     let htmlCard = document.createElement("img");
-    htmlCard.className = "border border-black border-2";
+    htmlCard.className = "border border-black border-2 ms-2 me-2 slide-in-right";
     htmlCard.src = card.imageSrc;
+    htmlCard.height = 225;
+    htmlCard.width = 150;
     document.getElementById("DealerSide").appendChild(htmlCard);
 }
 
-var ResultHtml = (result) => document.getElementById("Result").innerText = result;
+function ResultHtml(result) {
+    document.getElementById("Result").innerText = result;
+    document.getElementById("Hit").hidden = true;
+    document.getElementById("Stand").hidden = true;
+    document.getElementById("Result").parentElement.parentElement.className = "col-12 h-50 d-flex justify-content-center position-absolute top-50 start-50 translate-middle"
+    document.getElementById("canvas").style.opacity = 0.2
+    document.getElementById("Result").parentElement.className = "col-4 border border-2 border bg-white text-center animate__animated animate__fadeInDownBig"
+}
 
 function Shuffle() {
-    for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
+    for (let i = 0; i < Math.floor(Math.random() * 52); i++) {
         let deckCopy = deck.slice();
         let index = []
         deckCopy.forEach(x => index.push(deckCopy.indexOf(x)))
         deck.forEach(x => deckCopy[index.splice(Math.floor(Math.random() * index.length), 1)[0]] = x)
-        deck = deckCopy
+        deck = deckCopy.slice()
     }
 }
 
@@ -77,7 +97,8 @@ function UserHit() {
 function UserStand() {
     userStand = true;
     while (!endGame) {
-        DrawCard()
+        CheckPoints();
+        DrawCard();
     }
 }
 
@@ -103,14 +124,14 @@ function CheckPoints() {
     UserPointsHtml(userPointSum)
     DealerPointsHtml(dealerPointSum)
 
-    if (userPointSum > 21 || dealerPointSum == 21 || (dealerPointSum < 21 && dealerPointSum > userPointSum && userStand)) {
+    if (userPointSum > 21 || dealerPointSum == 21 || ((dealerPointSum <= 21 && dealerPointSum > userPointSum) && userStand)) {
         console.log("you lost")
-        ResultHtml("You lose!")
+        ResultHtml("You lost!")
         endGame = true
     }
     else if (userPointSum == 21 || (dealerPointSum > 21 && userPointSum <= 21)) {
         console.log("you won")
-        ResultHtml("You win!")
+        ResultHtml("You won!")
         endGame = true
     }
 }
